1d0d174d532401058598c477606de67140ad5a9e
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-31 23:27:57 -0300

    style(header): changing style position

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  width: 100%;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
   padding: 12px 20px;
 `;
