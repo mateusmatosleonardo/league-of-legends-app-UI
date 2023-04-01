4613307c4505f0ba869ed732c02ef52352e3f1ea
Mateus Leonardo <m4teusmatos@gmail.com>
2023-04-01 19:47:51 -0300

    style(theme): creating global theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  name: "DefaultTheme",
+  colors: {
+    WHITE: "#ffffff",
+    BLACK: "#0f0f0f",
+    GRAY: "#ececec",
+    YELLOW: "#cd8d04",
+  },
+  fonts: {
+    Montserrat_Regular: " Montserrat_400Regular",
+    Montserrat_Medium: " Montserrat_500Medium",
+    Montserrat_SemiBold: " Montserrat_600SemiBold",
+    Montserrat_Bold: " Montserrat_700Bold",
+  },
+};
