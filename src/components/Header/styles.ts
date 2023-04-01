b69f26239d76adec66a0cd51656026725d2ef4cb
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-31 21:39:09 -0300

    style(header): style definition for the header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 20px;
+`;
