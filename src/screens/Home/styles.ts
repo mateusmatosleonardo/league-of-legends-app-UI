306e1aec551e3a8bc03a4d4298cd643e363eee86
Mateus Leonardo <m4teusmatos@gmail.com>
2023-04-02 18:07:50 -0300

    style(home): refactoring home style

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,31 +6,10 @@ export const Title = styled.Text`
   font-size: ${RFValue(25.5)}px;
   color: ${({ theme }) => theme.colors.WHITE};
   text-align: center;
-  padding: 10px 0px;
+  padding: 16px 0px 10px 0px;
 `;
 
-export const TypesOfChampions = styled.View`
+export const EmptyContainer = styled.View`
   width: 100%;
-  padding: 6px 20px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const Type = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.Montserrat_SemiBold};
-  font-size: ${RFValue(15.5)}px;
-  color: ${({ theme }) => theme.colors.WHITE};
-`;
-
-export const HandleType = styled.Pressable`
-  justify-content: space-between;
-  align-items: center;
-  height: 30px;
-`;
-
-export const Hatch = styled.View`
-  width: 35px;
-  height: 3px;
-  background-color: ${({ theme }) => theme.colors.YELLOW};
+  height: 55px;
 `;
