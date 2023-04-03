039a73e887fb180cb1d0c14592d0ea5dbb0e0c5a
Mateus Leonardo <m4teusmatos@gmail.com>
2023-04-02 23:37:10 -0300

    chore: file to import and export images

---
+++ src/constants/images.ts
@@ -0,0 +1,17 @@
+import Jinx from "../assets/categories/marksmen/jinx.jpg";
+import Ashe from "../assets/categories/marksmen/ashe.jpg";
+import Caitlyn from "../assets/categories/marksmen/caitlyn.jpg";
+import Braum from "../assets/categories/tank/braum.jpg";
+import DrMundo from "../assets/categories/tank/drmundo.jpg";
+import Garen from "../assets/categories/tank/garen.jpg";
+import Janna from "../assets/categories/supports/janna.jpg";
+
+export default {
+  Jinx,
+  Ashe,
+  Caitlyn,
+  Braum,
+  DrMundo,
+  Garen,
+  Janna,
+};
