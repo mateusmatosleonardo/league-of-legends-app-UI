5a66a0cd5f934d9369b4582db461d1b2f3ec4d73
Mateus Leonardo <m4teusmatos@gmail.com>
2023-04-03 20:36:51 -0300

    style(header): changing positioning of header child components

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   padding: 6px 20px 18px 20px;
