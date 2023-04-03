08a1805db7dd44594a8b7d5159b294155050bec2
Mateus Leonardo <m4teusmatos@gmail.com>
2023-04-03 20:44:14 -0300

    feat: creating search component

---
+++ src/components/List/components/Search/Search.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Platform } from "react-native";
+import { BehaviorBox, Input, SearchIcon, WrapperIcon, WrapperInput, WrapperSearch } from "./styles";
+import { Feather } from '@expo/vector-icons';
+
+interface SearchProps {
+  value: string;
+  onChangeText: (text: string) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ value, onChangeText }: SearchProps) => {
+  return (
+    <WrapperSearch behavior={Platform.OS === 'ios' ? "padding" : 'height'}>
+      <WrapperInput>
+
+        <Input
+          placeholder="Pesquisar..."
+          placeholderTextColor="#ffffff"
+          value={value}
+          onChangeText={onChangeText}
+        />
+
+        <WrapperIcon>
+          <SearchIcon>
+            <Feather name="search" size={15} color="#ffffff" />
+          </SearchIcon>
+        </WrapperIcon>
+
+      </WrapperInput>
+
+      <BehaviorBox />
+    </WrapperSearch>
+  )
+}
+
+export default Search;
