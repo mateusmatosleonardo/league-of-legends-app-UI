eb9120da1b1a9f9be119f288d788e0fd2f286dc8
mateus <m4teusmatos@gmail.com>
2023-05-22 21:29:40 -0300

    fix: adjusting header layout

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,14 @@
+import { StatusBar } from "react-native";
 import styled from "styled-components/native";
+
+const statusBarHeight = StatusBar.currentHeight
+  ? StatusBar.currentHeight + 10
+  : 0;
 
 export const Container = styled.View`
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
   width: 100%;
-  padding: 6px 20px 18px 20px;
+  padding: ${statusBarHeight}px 20px 18px 20px;
 `;
