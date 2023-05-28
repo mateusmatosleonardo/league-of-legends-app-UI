23b4e9cb555d442cd4ee7f257c1fb3b7c07e26b1
mateus <m4teusmatos@gmail.com>
2023-05-28 20:46:42 -0300

    chore: adding new compononet in index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Layout from "./Layout/Layout";
 import Header from "./Header/Header";
 import List from "./List/List";
+import LoadingScreen from "./Loading/Loading";
 
-export { Layout, Header, List };
+export { Layout, Header, List, LoadingScreen };
