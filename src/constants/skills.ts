b48e56bbf3072c5848f0941c252ebafda7d4b35e
mateus <m4teusmatos@gmail.com>
2023-05-31 18:49:38 -0300

    feat: adding new icons

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,9 +1,18 @@
+// JINX
 import Passiva from "../assets/skills/jinx/passiva-anime-se.jpg";
 import Trocando from "../assets/skills/jinx/trocando.jpg";
 import Zap from "../assets/skills/jinx/zap.jpg";
 import MordidinhaFlamejante from "../assets/skills/jinx/mordidinha-flamejante.jpg";
 import SuperMegaMissilDaMorte from "../assets/skills/jinx/ult-mega-missil.jpg";
 import Atirador from "../assets/skills/jinx/Atirador.png";
+
+// VI
+import vi1 from "../assets/skills/vi/vi1.jpg";
+import vi2 from "../assets/skills/vi/vi2.jpg";
+import vi3 from "../assets/skills/vi/vi3.jpg";
+import vi4 from "../assets/skills/vi/vi4.jpg";
+import vi5 from "../assets/skills/vi/vi5.jpg";
+import Lutador from "../assets/skills/vi/Lutador.png";
 
 export default {
   Passiva,
@@ -12,4 +21,10 @@ export default {
   MordidinhaFlamejante,
   SuperMegaMissilDaMorte,
   Atirador,
+  vi1,
+  vi2,
+  vi3,
+  vi4,
+  vi5,
+  Lutador,
 };
