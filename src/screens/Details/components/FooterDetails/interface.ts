3d39f2cd11a0f3e89b75798bf21ab5c2e307dddf
mateus <m4teusmatos@gmail.com>
2023-05-31 14:57:45 -0300

    feat: creating type for footer details component

---
+++ src/screens/Details/components/FooterDetails/interface.ts
@@ -0,0 +1,8 @@
+import { ImageSourcePropType } from "react-native";
+
+export interface FooterDetailsProps {
+  icon: ImageSourcePropType;
+  roleOfChampion: string;
+  difficulty: string;
+  renderDifficulty: any;
+}
