858aa11e42262d117bf9c17a4eb870d7fd0991d9
mateus <m4teusmatos@gmail.com>
2023-06-02 14:48:44 -0300

    refactor: styled card

--- src/components/List/components/Card/styles.ts
+++ src/components/List/components/Card/styles.ts
@@ -1,59 +1,20 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const WrapperCard = styled.View`
-  flex-direction: row;
-  width: 100%;
-  height: 70px;
-  border-radius: 14px;
-  margin-bottom: 20px;
-`;
-
-export const WrapperImage = styled.View`
-  width: 70px;
-  height: 70px;
-`;
-
-export const ImageChampion = styled.Image`
-  width: 100%;
-  height: 100%;
-  border-top-left-radius: 14px;
-  border-bottom-left-radius: 14px;
-`;
-
-export const WrapperInfo = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  flex: 1;
-  border-top-right-radius: 14px;
-  border-bottom-right-radius: 14px;
-`;
-
-export const InfoChampion = styled.View`
-  flex: 1;
-  height: 100%;
-  justify-content: center;
-  padding-left: 20px;
-  border-top-right-radius: 14px;
-  border-bottom-right-radius: 14px;
-  background-color: ${({ theme }) => theme.colors.WHITE};
-`;
-
-export const NameChampion = styled.Text`
+export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.Montserrat_SemiBold};
-  font-size: ${RFValue(18.5)}px;
+  font-size: ${({ theme }) => RFValue(theme.sizes.lg)}px;
   color: ${({ theme }) => theme.colors.BLACK};
 `;
 
-export const TypeChampion = styled.Text`
+export const Type = styled.Text`
   font-family: ${({ theme }) => theme.fonts.Montserrat_Regular};
-  font-size: ${RFValue(12)}px;
+  font-size: ${({ theme }) => RFValue(theme.sizes.xxs) + 1.5}px;
   color: ${({ theme }) => theme.colors.BLACK};
-  margin-top: 6px;
+  margin-top: ${({ theme }) => theme.spacing.xxs + 2}px;
 `;
 
-export const ButtonDetails = styled.Pressable`
+export const GoDetails = styled.Pressable`
   position: absolute;
   bottom: 0;
   right: 0;
@@ -61,7 +22,7 @@ export const ButtonDetails = styled.Pressable`
   align-items: center;
   width: 35px;
   height: 35px;
-  border-top-left-radius: 14px;
-  border-bottom-right-radius: 14px;
+  border-top-left-radius: 10px;
+  border-bottom-right-radius: 10px;
   background-color: ${({ theme }) => theme.colors.YELLOW};
 `;
