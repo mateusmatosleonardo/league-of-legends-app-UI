fdf926c25a039c358ffa8589511ca1f85552e389
mateus <m4teusmatos@gmail.com>
2023-06-03 22:57:46 -0300

    changing title

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Layout, Header, List, CustomText } from '../../components';
+import { Layout, Header, List, } from '../../components';
 import { Logo } from '../../constants';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { theme } from '../../theme/theme';
+import { Title } from './styles';
 
 const HomeScreen: React.FC = () => {
-
-  const { colors, sizes, spacing, fonts } = theme;
 
   return (
     <Layout>
       <Header>
         <Logo width={38} height={38} />
       </Header>
-      <CustomText
-        color={colors.WHITE}
-        size={RFValue(sizes.xl) + 1.5}
-        fontFamily={fonts.Montserrat_SemiBold}
-        pt={spacing.md}
-        pb={spacing.sm}
-        alignment='center'
-      >
-        Selecione{' ' + ' '}seu{' ' + ' '}campeão
-      </CustomText>
+      <Title>Selecione{' ' + ' '}seu{' ' + ' '}campeão</Title>
       <List />
     </Layout >
   );
