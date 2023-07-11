e0b16e479ea1981475cea90eb326f59190beebb4
mateus <m4teusmatos@gmail.com>
2023-07-11 01:19:51 -0300

    style: create new style for list champions

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -1,8 +1,14 @@
-import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
-export const styles = StyleSheet.create({
-  list: {
-    paddingHorizontal: 20,
-    paddingBottom: 0,
-  },
-});
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Montserrat_SemiBold};
+  font-size: ${({ theme }) => RFValue(theme.sizes.lg)}px;
+  color: ${({ theme }) => theme.colors.WHITE};
+`;
+
+export const ShowAllText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Montserrat_Regular};
+  font-size: ${({ theme }) => RFValue(theme.sizes.xs)}px;
+  color: ${({ theme }) => theme.colors.GRAY_SECONDARY};
+`;
