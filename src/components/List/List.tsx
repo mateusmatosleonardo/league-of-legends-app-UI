4fc9354c7d4f5bcbda71b2fb1fe1d5ec3fcfed8e
mateus <m4teusmatos@gmail.com>
2023-07-11 01:19:15 -0300

    feat: i refactored the list interface

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,92 +1,53 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { FlatList, ListRenderItemInfo } from "react-native";
+import { MotiView } from "moti";
+import { CardProps, HomeScreenProps } from "../../interfaces/interfaces";
+import { useNavigation } from "@react-navigation/native";
+import { XStack, YStack } from "tamagui";
+import { ShowAllText, Title } from "./styles";
 import useChampionStore from "../../stores/useChampionStore";
 import Card from "./components/Card/Card";
-import OptionRow from "./components/OptionRow/OptionRow";
-import Search from "./components/Search/Search";
-import { styles } from "./styles";
-import { MotiView } from "moti";
-import { CardProps, HomeScreenProps, ListType } from "../../interfaces/interfaces";
-import { useNavigation } from "@react-navigation/native";
-
-const totalSpace: number = 90;
 
 const List: React.FC = () => {
 
   const { champions } = useChampionStore();
-
-  const [search, setSearch] = useState('');
-
-  const [select, setSelect] = useState<'all' | 'assassins' | 'fighters' | 'mages' | 'marksmen' | 'supports' | 'tanks'>('all');
 
   const navigation = useNavigation<HomeScreenProps>();
 
   const renderItem = useCallback(({ item, index }: ListRenderItemInfo<CardProps>) => {
     return (
       <MotiView
-        from={{ opacity: 0, translateX: 20 }}
+        from={{ opacity: 0, translateX: -30 }}
         animate={{
           opacity: 1,
           translateX: 0,
         }}
-        transition={{ delay: 380 + index * 380 }}
+        transition={{ delay: 400 + index * 400, type: "timing" }}
       >
         <Card {...item} navigation={() => navigation.navigate('DetailsScreen', { id: item.id })} />
       </MotiView>
     )
-  }, [])
-
-  const filteredAssassins = champions.filter(c => c.type.includes('assassins'));
-  const filteredFighters = champions.filter(c => c.type.includes('fighters'));
-  const filteredMages = champions.filter(c => c.type.includes('mages'));
-  const filteredMarksmen = champions.filter(c => c.type.includes('marksmen'));
-  const filteredSupports = champions.filter(c => c.type.includes('supports'));
-  const filteredTanks = champions.filter(c => c.type.includes('tanks'));
-  const filteredChampion = search.length > 0 ? champions.filter(c => c.name.includes(search)) : champions;
-
-  const SelectedFlatlist: React.FC<ListType> = ({ data }: ListType) => {
-    return (
-      <FlatList
-        contentContainerStyle={styles.list}
-        data={data.sort((a, b) => a.name.localeCompare(b.name))}
-        keyExtractor={item => item.name}
-        renderItem={renderItem}
-        getItemLayout={(data, index) => (
-          { length: totalSpace, offset: totalSpace * index, index }
-        )}
-      />
-    )
-  }
-
-  const CurrentFlatlist: React.FC = () => {
-    return (
-      <SelectedFlatlist
-        data={select === 'all' ?
-          champions : select === 'assassins' ?
-            filteredAssassins : select === 'fighters' ?
-              filteredFighters : select === 'mages' ?
-                filteredMages : select === 'marksmen' ?
-                  filteredMarksmen : select === 'supports' ?
-                    filteredSupports : select === 'tanks' ?
-                      filteredTanks : []} />
-    )
-  }
+  }, []);
 
   return (
-    <React.Fragment>
-      <OptionRow select={select} setSelect={setSelect} disabled={search.length > 0 ? true : false} />
-      {search.length > 0 ?
-        <FlatList
-          data={filteredChampion}
-          keyExtractor={item => item.name}
-          renderItem={renderItem}
-          contentContainerStyle={{ paddingHorizontal: 20 }}
-        /> : <CurrentFlatlist />}
-      <Search
-        value={search}
-        onChangeText={setSearch}
+    <YStack>
+      <XStack
+        justifyContent="space-between"
+        alignItems="center"
+        py="$5"
+        pl="$4"
+        pr="$4">
+        <Title>Meus campeões</Title>
+        <ShowAllText>Ver todos</ShowAllText>
+      </XStack>
+      <FlatList
+        data={champions}
+        keyExtractor={item => item.name}
+        renderItem={renderItem}
+        contentContainerStyle={{ paddingStart: 20, paddingEnd: 5 }}
+        horizontal
       />
-    </React.Fragment>
+    </YStack>
   )
 }
 
